525ed10741295f110b46f4a160d49d2de2d4e48d
wendrielypadilha <wendrielypadilhaa@gmail.com>
2024-11-28 16:14:35 -0300

    pizzas.js

---
+++ pizzas.js
@@ -0,0 +1,149 @@
+pizzaJson=[
+    {
+      id: 1,
+      name: 'Morango',
+      img: 'imagens/morango 2.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, morangos, essência de morango, recheio de brigadeiro de morango'
+    },
+    {
+      id: 2,
+      name: 'Chocolate',
+      img: 'imagens/chocolate.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, granulado de chocolate, essência , recheio de brigadeiro de chocolate'
+    },
+    {
+      id: 3,
+      name: 'Confetes',
+      img: 'imagens/confete.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, confetes, essência de baunilha, recheio de brigadeiro de baunilha'
+    },
+    {
+      id: 4,
+      name: 'Oréo',
+      img: 'imagens/oreo.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, bolacha oréo, essência de baunilha, recheio de brigadeiro branco com oréo'
+    },
+    {
+      id: 5,
+      name: 'Nutela',
+      img: 'imagens/nutela.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, nutela, essência de castanhas, recheio de brigadeiro de nutela'
+    },
+    {
+      id: 6,
+      name: 'Pistache',
+      img: 'imagens/pistache.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, pistache desidratado, essência de baunilha, recheio de brigadeiro de pistache'
+    },
+    {
+      id: 7,
+      name: 'Red velvet',
+      img: 'imagens/red.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, chantilly, essência de beterraba, recheio de nata'
+    },
+    {
+      id: 8,
+      name: 'Caramelo',
+      img: 'imagens/caramelo.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, essência de caju, recheio de brigadeiro de caramelo'
+    },
+    {
+      id: 9,
+      name: 'Cereja',
+      img: 'imagens/CEREJA.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, essência de cereja, recheio de brigadeiro branco, cerejas caramelizadas'
+    },
+    {
+      id: 10,
+      name: 'Kit Kat',
+      img: 'imagens/kit kat.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, essência de chocolate, recheio de brigadeiro, pedaços de kit kat'
+    },
+    {
+      id: 11,
+      name: 'Sensação',
+      img: 'imagens/sensacao.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, essência de morango, recheio de brigadeiro de morango, calda de morango '
+    },
+    {
+      id: 12,
+      name: 'Chocolate Branco',
+      img: 'imagens/branco.jpeg',
+      price: [15.00, 23.00, 27.00],
+      sizes: [
+        '',
+        '',
+        ''
+      ],
+      description: 'Massa, essência de baunilha, recheio de brigadeiro branco, chocolate derretido'
+    },
+
+    
+
+  ];
